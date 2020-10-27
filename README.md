8eead218c427347d98dd027dd4c4580c40ca592f
birdflu <birdflu@mail.ru>
2020-10-27 10:25:03 +0300

    advmute release 1.0

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# spotmute
-App mutes spotify's adverisement
+Spotify adblock.
+App mutes spotify's adverisement.
+
+```
+usage: python3 advmute.py [-h] [-o LOG_FILE_NAME]
+or daemon: advmute.py [-h] [-o LOG_FILE_NAME] &
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -o LOG_FILE_NAME, --out LOG_FILE_NAME
+                        log file
+```
--- advmute.py
+++ advmute.py
@@ -1,6 +1,8 @@
 import dbus
 import datetime
-
+import time
+import os
+from argparse import ArgumentParser
 
 def get_spotify_track_metadata():
     session_bus = dbus.SessionBus()
@@ -9,28 +11,67 @@ def get_spotify_track_metadata():
     spotify_properties = dbus.Interface(spotify_bus,
                                         "org.freedesktop.DBus.Properties")
     metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
-
+    
     return metadata
 
 
-def log(metadata):
+def log(log_file_name, metadata, delay):
     minutes = int((metadata['mpris:length'] / 1000000) // 60)
     seconds = int((metadata['mpris:length'] / 1000000) % 60)
     length = "" + str(minutes) + ":" + str(seconds)
-    
-    file_output = open("/tmp/output.txt", "a")
+
+    file_output = open(log_file_name, "a")
     print(datetime.datetime.today().strftime("%H:%M:%S"),
           metadata['xesam:autoRating'],
           metadata['xesam:discNumber'],
           metadata['xesam:trackNumber'],
           metadata['xesam:title'],
           metadata['mpris:length'],
-          length, file=file_output)
+          length,
+          delay,
+          file=file_output)
     file_output.close()
 
-def run(count):
-    while count > 0:
-        log(get_spotify_track_metadata())
-        count -= 1  
+def log_header(log_file_name):    
+    file_output = open(log_file_name, "w")
+    print('sysdate',
+          'autoRating',
+          'discNumber',
+          'trackNumber',
+          'title',
+          'length(ms)',
+          'length(mi:ss)',
+          'delay',
+          file=file_output)
+    file_output.close()
+ 
 
-run(3)
+def run():
+
+    parser = ArgumentParser()
+    parser.add_argument('-o', "--out", action="store", dest="log_file_name", default='advmute.log', type=str, help='log file')
+    args = parser.parse_args()
+
+    log_header(args.log_file_name)
+   
+    currentTrack = ''
+    delay = 1;
+    while True:
+        metadata = get_spotify_track_metadata()
+        if metadata['xesam:title'] == 'Advertisement':
+            os.system("amixer -D pulse sset Master mute")
+            time.sleep(31)
+            os.system("amixer -D pulse sset Master unmute")
+        else:
+            if currentTrack == metadata['xesam:title']:
+                delay = delay // pow(metadata['mpris:length'] / 1000000, (1/8))
+            else:
+                if currentTrack != '' :
+                    delay = int(metadata['mpris:length'] / 1000000) // 2 
+                currentTrack = metadata['xesam:title']
+        if delay < 1:
+            delay = 1
+        log(args.log_file_name, metadata, delay)
+        time.sleep(delay)
+
+run()
